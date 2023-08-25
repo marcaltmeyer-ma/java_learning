ac1aa6df106a38d751bd385c9c29c76acc8f838f
altmeyma <altmeyer_marc@gmx.de>
2023-08-25 14:05:06 +0200

    trying to add handing in the full word

--- Hangman/src/Main.java
+++ Hangman/src/Main.java
@@ -38,7 +38,7 @@ public class Main {
 			}
 
 			// MAX That's a nice and concise main function.
-		}
+		} // while(replay)
 		scanner.close();
 	}
 
@@ -60,7 +60,7 @@ public class Main {
 		replay = enterLetter(word, solution, lives, replay);
 		// }
 		return replay;
-	}
+	}// localMultiplayer
 
 	// Starts single player with a random word
 	public static boolean singleplayer(boolean replay) {
@@ -76,7 +76,7 @@ public class Main {
 		replay = enterLetter(word, solution, lives, replay);
 		// }
 		return replay;
-	}
+	}// singleplayer
 
 	// The function for the first player entering the word
 	public static char[] enterWord() {
@@ -84,7 +84,7 @@ public class Main {
 		char[] word;
 		word = cons.readPassword();
 		return word;
-	}
+	}// enterWord
 
 	// Opening Word list
 	public static List<String> openWordList() {
@@ -95,19 +95,19 @@ public class Main {
 			System.out.println("WordList fuckup");
 		}
 		return words;
-	}
+	}// openWordList
 
 	// A helping function to get the Hangman-typical _-notation of the word
 	public static char[] getArray(String solu) {
 		char[] word = new char[solu.length()];
 		Arrays.fill(word, '_');
 		return word;
-	}
+	}// getArray
 
 	// The function if the player asks for the words' length
 	public static void askLength(char[] word) {
 		System.out.println("The word has" + " " + word.length + " " + "tokens.");
-	}
+	}// askLength
 
 	// The function to return the rules if the player asks
 	public static void askRules() {
@@ -116,17 +116,17 @@ public class Main {
 		System.out.println("By entering '*', you can see how many lives you have left.");
 		System.out.println("To see the letters that were guessed wrong, enter '-'.");
 		System.out.println("To see these rules again, enter '?'.");
-	}
+	}// askRules
 
 	// Gives out the lives the player has left
 	public static void askLives(int lives) {
 		System.out.println("You have " + lives + " lives left.");
-	}
+	}// askLives
 
 	// Prints the list of wrong tokens
 	public static void askWrong(char[] wrg) {
 		System.out.println(wrg);
-	}
+	}// askWrong
 
 	// Ask the player if they want to play again
 	// MAX trailing whitespace (well, before I put the comment on the line ^^)
@@ -142,7 +142,7 @@ public class Main {
 			return false;
 		}
 		return false;
-	}
+	}// askAgain
 
 	// MAX for these kinds of "experiments" I would rather use git branches than
 	// commenting
@@ -156,53 +156,55 @@ public class Main {
 		int idx = 0;
 		boolean guessedCharInWord = false;
 		boolean guessedCharInWrong = false;
-
+		String inputWord = scanner.next();
 		// MAX debug output in release version
+		if (inputWord.length() == 1) {
 		while (!String.valueOf(word).equals(solution) && lives > 0) {
-			char token = scanner.next().charAt(0);
-			boolean helper = false;
-			if (token == '#') {
-				askLength(word);
-			} else if (token == '*') {
-				askLives(lives);
-			} else if (token == '-') {
-				askWrong(wrong);
-			} else if (token == '?') {
-				askRules();
-			} else if (token == '=') {// TODO Debug line! Delete!
-				System.out.println(solution);
-			} else if ((guessedCharInWrong = Checker.checker(token, wrong))
-					|| (guessedCharInWord = Checker.checker(token, word))) {
-				System.out.println(token
-						+ " has already been guessed. Press - to check the wrongly guessed letters, or = to see the word so far.");
-			} else {
-				for (; idx < solution.length(); idx++) {
-					if (token == solution.charAt(idx)) {
-						helper = true; // MAX not quite sure what helper is used for. Is it just to
-						// MAX say that the current char is in the word?
-						// MAX You might want to use a better name here.
-						System.out.println(token + " " + "is in the word!");
-						word[idx] = token;
-						System.out.println("word: " + String.valueOf(word));
-						// MAX one important element of hangman isn't implmented yet:
-						// MAX You don't show where the blanks in the word are.
-						// MAX Let's say the word is "messer" and the user guesses "e"
-						// MAX then your word would show as "ee". It would be nice if it
-						// MAX showed as "_e__e_".
+			// char token = scanner.next().charAt(0);
+				char token = inputWord.charAt(0);
+				boolean helper = false;
+				if (token == '#') {
+					askLength(word);
+				} else if (token == '*') {
+					askLives(lives);
+				} else if (token == '-') {
+					askWrong(wrong);
+				} else if (token == '?') {
+					askRules();
+				} else if (token == '=') {// TODO Debug line! Delete!
+					System.out.println(solution);
+				} else if ((guessedCharInWrong = Checker.checker(token, wrong))
+						|| (guessedCharInWord = Checker.checker(token, word))) {
+					System.out.println(token
+							+ " has already been guessed. Press - to check the wrongly guessed letters, or = to see the word so far.");
+				} else {
+					for (; idx < solution.length(); idx++) {
+						if (token == solution.charAt(idx)) {
+							helper = true; // MAX not quite sure what helper is used for. Is it just to
+							// MAX say that the current char is in the word?
+							// MAX You might want to use a better name here.
+							System.out.println(token + " " + "is in the word!");
+							word[idx] = token;
+							System.out.println("word: " + String.valueOf(word));
+							// MAX one important element of hangman isn't implmented yet:
+							// MAX You don't show where the blanks in the word are.
+							// MAX Let's say the word is "messer" and the user guesses "e"
+							// MAX then your word would show as "ee". It would be nice if it
+							// MAX showed as "_e__e_".
 
+						}
 					}
-				}
-				if (idx >= solution.length() && helper == false) {
-					// System.out.println("End of word");
-					System.out.println(token + " is not in the word!");
-					lives--;
-					System.out.println("You have " + lives + " lives left.");
-					wrong[i] = token;
-					i++;
-				}
+					if (idx >= solution.length() && helper == false) {
+						// System.out.println("End of word");
+						System.out.println(token + " is not in the word!");
+						lives--;
+						System.out.println("You have " + lives + " lives left.");
+						wrong[i] = token;
+						i++;
+					}
 
-				// MAX this if statement kind of seems redundant...
-				// MAX Does it do something different from the one above?
+					// MAX this if statement kind of seems redundant...
+					// MAX Does it do something different from the one above?
 //				if (idx > solution.length()) {
 //					System.out.println("This letter is not in the word.");
 //					lives--;
@@ -210,10 +212,15 @@ public class Main {
 //					wrong[i] = token;
 //					i++;
 //				}
-				idx = 0;
+					idx = 0;
 
-			}//else
-		} //while
+				}
+			} 
+		} else if (inputWord.equals(solution)) {
+			System.out.println("It works!");
+		} else {
+			System.out.println("Wrong word!");
+		} 
 		if (String.valueOf(word).equals(solution)) {
 			System.out.println("You are correct! The word is " + solution);
 			System.out.println("You had " + lives + " lives left.");
@@ -225,5 +232,5 @@ public class Main {
 			return replay;
 		}
 		return replay;
-	}
+	}// enterLetter
 }
