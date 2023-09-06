29df178fba84fadeb91639914ee88f2d0e9031f6
altmeyma <altmeyer_marc@gmx.de>
2023-09-06 15:46:35 +0200

    Initializing field

--- 2048/src/game2048/Board.java
+++ 2048/src/game2048/Board.java
@@ -5,10 +5,13 @@ import java.util.List;
 import java.util.Random;
 
 public class Board {
-	public static void InitField() {
+	this.boardTest = new Tile[][];
+	
+	public void InitField() {
 	List<Integer> initList = Arrays.asList(2,4); //A new tile can either be a 2 or a 4, so we need a list to chose from
 	Tile gameboard[][] = new Tile[4][4]; //Initialize the game board as a 4x4 matrix
 	Random rand = new Random(); //from https://www.baeldung.com/java-random-list-element
+	
 	
 	
 	
@@ -16,6 +19,17 @@ public class Board {
 	int x = new Random().nextInt(4);
 	int y = new Random().nextInt(4); //x and y are coordinates in the field. When a new tile gets filled, we need a random x and y coordinate, on which the 2 or 4 appears
 	int newTile = initList.get(rand.nextInt(initList.size()));
-	System.out.println(newTile);
+	//System.out.println("New tile:" + newTile);
+	
+	gameboard[x][y].val = newTile;
+	gameboard[x][y].occupied = true;
+	}
+	
+	public void PrintBoard(Board board) { //https://stackoverflow.com/questions/7782080/how-to-print-a-two-dimensional-array
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				System.out.println(board[j][i]);
+			}
+		}
 	}
 }
